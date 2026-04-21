a6a436afdbe47d15b2c7a1c5ee390578e9a2825b
Nirmani Yasasiri <nirmaniyasasiri@gmail.com>
2026-04-22 02:16:36 +0530

    Add comments to LinkedResourceNotFoundException

--- src/main/java/com/smartcampus/exception/LinkedResourceNotFoundException.java
+++ src/main/java/com/smartcampus/exception/LinkedResourceNotFoundException.java
@@ -1,8 +1,7 @@
 package com.smartcampus.exception;
 
 /**
- * Thrown when a request refers to another resource
- * that does not exist, such as a sensor using an invalid roomId.
+ * Thrown when a request refers to another resource that does not exist, such as a sensor using an invalid roomId.
  */
 public class LinkedResourceNotFoundException extends RuntimeException {
 
